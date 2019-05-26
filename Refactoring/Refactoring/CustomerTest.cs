f2174d1aa2beea0f4dc63397d1670249141c7fd1
Ronaldo <ronaldolucas_gc@hotmail.com>
2019-05-26 17:52:51 -0300

    Testes de unidade

---
+++ Refactoring/Refactoring/CustomerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactoring
+{
+    [TestClass()]
+    public class CustomerTest
+    {
+        [TestMethod()]
+        public void statementTest()
+        {
+            Movie KillBill = new Movie("Kill Bill - Vol 1", Movie.REGULAR);
+            Movie FindingNemo = new Movie("Procurando Nemo", Movie.CHILDRENS);
+            Movie Avengers = new Movie("Vingadores - Guerra Infinita", Movie.NEW_RELEASE);
+
+            Customer cliente = new Customer("Fulano");
+
+            cliente.addRental(new Rental(KillBill, 2));
+            cliente.addRental(new Rental(FindingNemo, 2));
+
+            string estado = cliente.statement();
+
+            Assert.IsTrue(estado.Contains("Fulano"));
+            Assert.IsTrue(estado.Contains("3.5"));
+        }
+    }
+}
